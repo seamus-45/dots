2d65c13a50952bf555dbd3628ab99e5a0f7e2175
seamus-45 <seamus-45@protonmail.com>
2024-11-07 13:05:26 +0500

    change zsh extras dir

---
+++ dotfiles/config/zsh/profile.d/garuda-seamus.zsh
@@ -0,0 +1,7 @@
+# Connection helpers
+alias lxcL='ssh -t seamus-lxc tmux -u new-session -A -s lxc-L'
+alias lxcR='ssh -t seamus-lxc tmux -u new-session -A -s lxc-R'
+
+# Garuda-home vars
+export MPD_HOST="/home/seamus/.mpd/socket"
+export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true"
---
+++ dotfiles/config/zsh/profile.d/seamus-lxc.zsh
@@ -0,0 +1,10 @@
+# Work helpers
+alias ssh="signmykey -e; ssh"
+alias scp="signmykey -e; scp"
+alias tmuxp="signmykey -e; tmuxp"
+alias ansible="signmykey -e; ansible"
+alias ansible-playbook="signmykey -e; ansible-playbook"
+alias htop='gotop'
+
+# Work vars
+export VAULT_ADDR=https://vault.maxim.services:8200
