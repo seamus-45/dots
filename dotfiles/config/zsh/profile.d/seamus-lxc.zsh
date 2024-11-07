a78a31635b2b939b03bbad3c9f1543f29e712907
seamus-45 <seamus-45@protonmail.com>
2024-11-07 13:50:37 +0500

    update seamus-lxc.sh

--- dotfiles/config/zsh/profile.d/seamus-lxc.zsh
+++ dotfiles/config/zsh/profile.d/seamus-lxc.zsh
@@ -5,6 +5,26 @@ alias tmuxp="signmykey -e; tmuxp"
 alias ansible="signmykey -e; ansible"
 alias ansible-playbook="signmykey -e; ansible-playbook"
 alias htop='gotop'
+# usage: cat file.yml | yaml2json
+alias yaml2json="python3 -c 'import sys, yaml, json; json.dump(yaml.safe_load(sys.stdin), sys.stdout, indent=2)'"
+
+# Vault helpers
+vault_ldap_login() { vault login -no-print -method=ldap username=$USER }
+vault_root_password() {	vault kv get -field 2023_root unix/ru/ssh | copy2cb }
+vault_root_password_ir() { vault kv get -field root unix/ir/ssh | copy2cb }
+vault_my_token() { cat ~/.vault-token | copy2cb }
+
+# Functions
+copy2cb() {
+	if [ -z "$SSH_CLIENT" ]; then
+    xclip -selection clipboard
+	else
+    # OSC52 terminal sequence
+    base64 | xargs printf $'\e]52;c;%s\a'
+	fi
+}
 
 # Work vars
 export VAULT_ADDR=https://vault.maxim.services:8200
+export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
+export TECHNO_REGIONS='ru ir sg id br cl'
