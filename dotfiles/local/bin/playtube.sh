7aa25048759aeaf111c81f10dfa2cfbba096468e
seamus-45 <fedotov_sv@taximaxim.ru>
2024-12-04 14:38:49 +0500

    add audio mode for playtube.sh

--- dotfiles/local/bin/playtube.sh
+++ dotfiles/local/bin/playtube.sh
@@ -1,10 +1,31 @@
 #!/bin/sh
 if [ $# -lt 1 ]; then
-  echo "Usage: $(basename $0) URL"
+  echo "Usage: $(basename $0) [-a] URL"
+  echo "Args:"
+  echo "  -a  Play audio only"
   exit 1
 fi
 
+OPTSTRING=":a"
+while getopts ${OPTSTRING} opt; do
+  case ${opt} in
+    a)
+      AUDIO=1
+      shift
+      ;;
+    ?)
+      echo "Unknown argument: -${OPTARG}"
+      exit 1
+      ;;
+  esac
+done
+
 URL=$1
 
-yt-dlp -q "${URL}" --proxy socks5://127.0.0.1:1080 -o - | mpv -force-seekable=yes --autofit=1920 -really-quiet - &
-  # mpv -force-seekable=yes --autofit=1920 -really-quiet --x11-name="pip" - &
+if [ ${AUDIO:-0} -eq 1 ];
+then
+  yt-dlp -q "${URL}" --proxy socks5://127.0.0.1:1080 -f 'bestaudio[ext=m4a]' -o - | mpv -force-seekable=yes -really-quiet -force-window=yes - &
+else
+  # --x11-name="pip"
+  yt-dlp -q "${URL}" --proxy socks5://127.0.0.1:1080 -o - | mpv -force-seekable=yes --autofit=1920 -really-quiet - &
+fi
