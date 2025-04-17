4f59e8e12820c949994bdae998fbcc822c6c1207
seamus-45 <seamus-45@protonmail.com>
2025-04-17 18:43:24 +0500

    improve vault_ldap_login helper

--- dotfiles/config/zsh/profile.d/seamus-lxc.zsh
+++ dotfiles/config/zsh/profile.d/seamus-lxc.zsh
@@ -9,7 +9,11 @@ alias htop='gotop'
 alias yaml2json="python3 -c 'import sys, yaml, json; json.dump(yaml.safe_load(sys.stdin), sys.stdout, indent=2)'"
 
 # Vault helpers
-vault_ldap_login() { vault login -no-print -method=ldap username=$USER }
+vault_ldap_login() {
+  # get token age in hours
+  [ -s ~/.vault-token ] && local token_age=$((($(date +%s) - $(date +%s -r ~/.vault-token)) / 3600)) || local token_age=12
+  test $token_age -ge 12 && vault login -no-print -method=ldap username=$USER ||:
+}
 vault_root_password() {	vault kv get -field 2023_root unix/ru/ssh | copy2cb }
 vault_root_password_ir() { vault kv get -field root unix/ir/ssh | copy2cb }
 vault_my_token() { cat ~/.vault-token | copy2cb }
