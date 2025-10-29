ab2f790d4c737cbb9f5696676d61a901f3b1978a
seamus-45 <seamus-45@proton.me>
2025-10-29 20:32:41 +0500

    zshrc: move fastfetch to separate profile

--- dotfiles/config/zsh/profile.d/seamus-pad.zsh
+++ dotfiles/config/zsh/profile.d/seamus-pad.zsh
@@ -8,3 +8,8 @@ lxc() {
 # Garuda-home vars
 export MPD_HOST="/home/seamus/.mpd/socket"
 export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true"
+
+## Show system info
+if command -v fastfetch; then
+  fastfetch --config mokka.jsonc
+fi
