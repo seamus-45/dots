157af62d12ae6f3682752d841819bdb6fd96504b
seamus-45 <seamus-45@proton.me>
2025-11-07 17:24:46 +0500

    neovim: add html prettier to conform

--- dotfiles/config/nvim/lua/configs/conform.lua
+++ dotfiles/config/nvim/lua/configs/conform.lua
@@ -2,6 +2,7 @@ local options = {
   formatters_by_ft = {
     lua = { "stylua" },
     yaml = { "prettier" },
+    html = { "prettier" },
     -- css = { "prettier" },
     -- html = { "prettier" },
   },
