116325cea8662d4b3b1e012b1a7d91a6a19781e3
seamus-45 <seamus-45@proton.me>
2025-12-17 16:53:46 +0500

    zsh: add https proxy for pacman/paru

--- dotfiles/config/zsh/profile.d/garuda-seamus.zsh
+++ dotfiles/config/zsh/profile.d/garuda-seamus.zsh
@@ -8,5 +8,15 @@ lxc() {
 # Garuda-home vars
 export MPD_HOST="/home/seamus/.mpd/socket"
 export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true"
+
 # Fix bug with drop down menus not clickable
 export _JAVA_AWT_WM_NONREPARENTING=1
+
+## Show system info
+if command -v fastfetch; then
+  fastfetch --config mokka.jsonc
+fi
+
+# Fix cellular network problems
+alias pacman="env https_proxy=socks://127.0.0.1:1080 pacman"
+alias paru="env https_proxy=socks://127.0.0.1:1080 paru"
