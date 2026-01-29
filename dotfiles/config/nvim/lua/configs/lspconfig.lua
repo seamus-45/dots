fd3f386ee83474b759e5e16bde5777320d475b8a
seamus-45 <seamus-45@protonmail.com>
2026-01-29 10:15:22 +0500

    neovim: reconfigure lsp

--- dotfiles/config/nvim/lua/configs/lspconfig.lua
+++ dotfiles/config/nvim/lua/configs/lspconfig.lua
@@ -1,9 +1,6 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
-local configs = require "nvchad.configs.lspconfig"
-
 local servers = {
   pylsp = {
     settings = {
@@ -27,9 +24,6 @@ local servers = {
 }
 
 for name, opts in pairs(servers) do
-  opts.on_init = configs.on_init
-  opts.on_attach = configs.on_attach
-  opts.capabilities = configs.capabilities
-
-  lspconfig[name].setup(opts)
+  vim.lsp.config(name, opts)
+  vim.lsp.enable(name)
 end
